ba2eb78dfd679bc742c53ea4a468a1feabe03fa2
coderAndrew <omolloandrew37@gmail.com>
2025-04-24 12:34:11 +0300

    creating data in prisma

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/prisma/client";
+import schema from "./schema";
 
 interface User {
   id: number;
@@ -11,7 +12,35 @@ export async function GET(request: NextRequest) {
   return NextResponse.json(users as User[]);
 }
 
-export async function POST(request: NextRequest): Promise<NextResponse<User>> {
-  const { name } = await request.json();
-  return NextResponse.json({ id: Math.random(), name } as User);
+export async function POST(
+  request: NextRequest
+): Promise<NextResponse<User | { error: string }>> {
+  const body = await request.json();
+  const validationResult = schema.safeParse(body);
+  if (!validationResult.success) {
+    return NextResponse.json(
+      { error: validationResult.error.message },
+      { status: 400 }
+    );
+  }
+
+  // First check if user exists
+  const existingUser = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (existingUser) {
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+  }
+
+  // If no existing user, create new one
+  const user = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  return NextResponse.json(user as User, { status: 201 });
 }
--- app/api/users/schema.ts
+++ app/api/users/schema.ts
@@ -1,7 +1,18 @@
 import { z } from "zod";
 
 const schema = z.object({
-  name: z.string(),
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters long" }),
+  email: z
+    .string()
+    .email({ message: "Invalid email address" })
+    .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+      message: "Invalid email format",
+    }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
 });
 
 export default schema;
